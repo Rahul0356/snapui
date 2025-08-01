d9190d5de22e06c77fa78383799f1767f0e1da9d
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-08-01 09:54:34 +0530

    add the button and icons

---
+++ components/providers/Features.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Nextjs from '../icons/nextjs';
+import ReactIcon from '../icons/react';
+import ShadcnIcon from '../icons/shadcn';
+import Motion from '../icons/motion';
+
+
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default Features
